e6aec6d5b6c5f3fe19ea13253124b6dae46bcda5
Aman Sultan Baig <amansultanbaig@gmail.com>
2019-12-12 23:54:11 -0800

    asynchronous approach implemented

--- file_system.js
+++ file_system.js
@@ -1,3 +1,5 @@
+//synchronous approach
+
 const fs = require('fs');
 let fileName = (__filename);
 
@@ -9,4 +11,14 @@ console.log(`'${makeFile}' is written in this File!`);
 
 let fileExist = fs.unlinkSync('writeFile.txt');
 if(!makeFile) console.log(`File dosen't exists`);
-else console.log(`File DELETED Successfully!`)
+else console.log(`File DELETED Successfully!`);
+
+// asynchronous approach
+
+let writefile = fs.writeFile('writeFile.txt','Aman Sultan Baig with asynchronous',(err) => {
+  (err) ? console.log(err) : console.log('File created with Asynchronous Approach')
+});
+
+let FileRead = fs.readFile('writeFile.txt','utf-8',(err, content) => {
+  (err) ? console.log(err) : console.log(content)
+});
